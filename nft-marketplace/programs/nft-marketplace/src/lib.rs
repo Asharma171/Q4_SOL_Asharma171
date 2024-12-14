07bd2bf2a512ce3eafca2b1a897512abaf1a118a
Asharma171 <ankitsharma1712000@gmail.com>
2024-12-14 12:54:27 +0530

    marketplace program

---
+++ nft-marketplace/programs/nft-marketplace/src/lib.rs
@@ -0,0 +1,32 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("BRV4ZAkfVphrMoUy8jWjLaGd1qS1pw4EcwHJ6Sk8H7mi");
+
+#[program]
+pub mod nft_marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)
+    }
+
+    pub fn list(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()
+    }
+
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.send_sol()?;
+        ctx.accounts.send_nft()?;
+        ctx.accounts.close_mint_vault()
+    }
+}
